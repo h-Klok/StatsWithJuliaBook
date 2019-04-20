9634e223edf828db29f29638b7c689801e111e29
h-Klok <h.klok@business.uq.edu.au>
2019-04-21 02:46:44 +1000

    modifications

--- 9_chapter/predatorPrey.jl
+++ 9_chapter/predatorPrey.jl
@@ -14,8 +14,8 @@ for t in 2:tEnd
     traj[t] = next(traj[t-1]...)
 end
 
-plot(first.(traj),last.(traj),"k--")
-plot(first.(traj),last.(traj),"r.")
-plot(equibPoint[1],equibPoint[2],"g.",ms="10")
-xlabel("X"); ylabel("Y")
-xlim(0,1);ylim(0,0.3);
+plot(first.(traj),last.(traj),"k--",label="Model trajectory")
+plot(first.(traj),last.(traj),"b.",label="Discrete states \nof model")
+plot(equibPoint[1],equibPoint[2],"r+",mew="4",ms="10",label="Equlibrium point")
+xlabel("X1"); ylabel("X2"); legend(loc="upper right")
+xlim(0,1); ylim(0,0.3)
--- 9_chapter/springMass.jl
+++ 9_chapter/springMass.jl
@@ -11,19 +11,19 @@ tRange = 0:0.1:tEnd
 manualSol = [exp(A*t)*initX for t in tRange]
 
 linearRHS(x,Amat,t) = Amat*x 
-prob = ODEProblem(linearRHS, initX, (0,tEnd),A)
+prob = ODEProblem(linearRHS, initX, (0,tEnd), A)
 sol = solve(prob)
 
 figure(figsize=(10,5))
 subplot(121)
 xlim(-7,9);ylim(-9,7)
-plot(first.(manualSol),last.(manualSol),"b")
-plot(first.(sol.u),last.(sol.u),"r.")
-plot(initX[1],initX[2],"g.",ms="15")
-xlabel("Displacement"); ylabel("Velocity")
+plot(first.(manualSol),last.(manualSol),"b", label="Manual trajectory")
+plot(first.(sol.u),last.(sol.u),"r.", label="DiffEq package")
+plot(initX[1],initX[2],"k.",ms="15",label="Initial state")
+xlabel("Displacement"); ylabel("Velocity"); legend(loc="upper right")
 
 subplot(122)
-plot(tRange,first.(manualSol),"b")
-plot(sol.t,first.(sol.u),"r.")
-plot(0,initX[1],"g.",ms="15");
-xlabel("Time"); ylabel("Displacement")
+plot(tRange,first.(manualSol),"b", label="Manual trajectory")
+plot(sol.t,first.(sol.u),"r.", label="DiffEq package")
+plot(0,initX[1],"k.",ms="15",label="Initial state");
+xlabel("Time"); ylabel("Displacement"); legend(loc="upper right")
