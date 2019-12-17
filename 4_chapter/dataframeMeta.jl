33e6c234f18c9a3a53abf0bc92197449bcf0a37d
h-Klok <h.klok@business.uq.edu.au>
2019-12-18 00:25:24 +1000

    Various updates incl Plots change

--- 4_chapter/dataframeMeta.jl
+++
@@ -1,7 +0,0 @@
-include("dataframeCreation.jl")
-
-using DataFramesMeta
-@where(dropmissing(purchaseData), :Price .> 10000, :Type .== "A")
-
-grouped = groupby(dropmissing(purchaseData), :Type)
-@based_on(grouped, avgPrice = mean(:Price))
