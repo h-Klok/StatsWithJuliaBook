16c2c2f183f97b10f0d7991f25d0565d29d88f95
h-Klok <h.klok@business.uq.edu.au>
2019-12-18 00:57:42 +1000

    Various updates incl Plots change

---
+++ 4_chapter/dataBoxPlot.jl
@@ -0,0 +1,9 @@
+using CSV, StatsPlots; pyplot()
+
+data1 = CSV.read("../data/machine1.csv", header=false)[:,1]
+data2 = CSV.read("../data/machine2.csv", header=false)[:,1]
+data3 = CSV.read("../data/machine3.csv", header=false)[:,1]
+
+boxplot([data1,data2,data3], c=[:blue :red :green], label="",
+        xticks=([1:1:3;],["1", "2", "3"]), xlabel="Machine type",
+        ylabel="Pipe Diameter (mm)")
