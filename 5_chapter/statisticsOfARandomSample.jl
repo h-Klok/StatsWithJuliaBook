bec24c6fac87127b5fc19e570c83fbb50a1698db
h-Klok <h.klok@business.uq.edu.au>
2019-12-18 22:53:32 +1000

    Various updates incl Plots change

--- 5_chapter/statisticsOfARandomSample.jl
+++ 5_chapter/statisticsOfARandomSample.jl
@@ -1,4 +1,4 @@
-using Random, Distributions, PyPlot
+using Random, Distributions, Plots; pyplot()
 Random.seed!(0)
 
 lambda  = 1/4.5
@@ -14,13 +14,13 @@ for i in 1:N
     variances[i] = var(data)
 end
 
-plt.hist(means,200, color="b", label = "Histogram of Sample Means",
-			histtype = "step", density = true)
-plt.hist(variances,600, color="r", label = "Histogram of Sample Variances",
-			histtype = "step", density = true)
-xlim(0,40)
-legend(loc="upper right")
 println("Actual mean: ",mean(expDist),
 		"\nMean of sample means: ",mean(means))
 println("Actual variance: ",var(expDist),
 	"\nMean of sample variances: ",mean(variances))
+
+stephist(means, bins=200, c=:blue, normed=true, 
+	label="Histogram of Sample Means")
+stephist!(variances, bins=600, c=:red, normed=true, 
+	label="Histogram of Sample Variances", xlims=(0,40), ylims=(0,0.4),
+    	xlabel = "Statistic value", ylabel = "Density")
