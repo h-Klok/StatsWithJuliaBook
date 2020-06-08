1e5799f93d83355974b78712955259c53084fdbe
Yoni Nazarathy <yoninazarathy@gmail.com>
2020-06-08 15:46:50 +1000

    update with output

--- 9_chapter/simpleSVM.jl
+++ 9_chapter/simpleSVM.jl
@@ -1,35 +1,30 @@
 using Flux.Data.MNIST, LIBSVM, Plots
 
-imgs   = MNIST.images()
-labels = MNIST.labels()
+logFilePath = "../data/svmlog.txt"
 
-trainData = hcat([vcat(float.(imgs[i])...) for i in 1:1000]...)
-trainLabels = labels[1:1000]
+trainImgs   = MNIST.images()[1:10000]
+trainLabels = MNIST.labels()[1:10000]
+nTrain = length(trainImgs)
+trainData = hcat([vcat(float.(trainImgs[i])...) for i in 1:nTrain]...)
 
-testData = hcat([vcat(float.(imgs[i])...) for i in 1001:2000]...)
-testLabels = labels[1001:2000]
+testImgs = MNIST.images(:test)
+testLabels = MNIST.labels(:test)
+nTest = length(testImgs)
+testData = hcat([vcat(float.(testImgs[i])...) for i in 1:nTest]...)
 
-model = svmtrain(trainData,trainLabels)
-
-(predicted_labels, decision_values) = svmpredict(model, testData)
-
-accuracy = sum(predicted_labels .== testLabels)/1000
-println("Prediction accuracy (measured on test set of size 1000): ", accuracy)
-
-showImages = float.(imgs[1001:1010])
-matshow(hcat(showImages...), cmap="Greys")
-for i in 1:10
-    ok = predicted_labels[i] == testLabels[i] ? "" : "x"
-    annotate("$(predicted_labels[i])$(ok)", xy=(28i-10,25), xytext=(28i-10, 25),
-    		bbox=Dict("fc"=>"0.8"))
+@info "Training model with verbose output to $logFilePath."
+@time begin
+    sOut = stdout
+    logF = open(logFilePath, "w")
+    redirect_stdout(logF)
+    model = svmtrain(trainData, trainLabels, 
+                    kernel = Kernel.Linear, verbose=true)
+    close(logF)
+    redirect_stdout(sOut)
+    @info "Training complete."
 end
 
-QQQQ breaking
-p1 = heatmap(showImages[1])
-p2 = heatmap(showImages[2])
-p3 = heatmap(showImages[3])
-p4 = heatmap(showImages[4])
+predicted_labels, _ = svmpredict(model, testData)
 
-plot(p1, p2, p3, p4, layout=(1,4), color=:Greys)
-
-default(color=:Greys, colorbar_entry=false, ratio=:equal, xaxis=false, yaxis=false)
+accuracy = sum(predicted_labels .== testLabels)/nTest
+println("Prediction accuracy (measured on test set of size $nTest): ", accuracy)
