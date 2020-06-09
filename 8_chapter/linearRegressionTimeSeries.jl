2b34ead5604808ef7f5a3beb0169ab78bb0d3628
Yoni Nazarathy <yoninazarathy@gmail.com>
2020-06-09 16:31:41 +1000

    time series examples

--- 8_chapter/linearRegressionTimeSeries.jl
+++ 8_chapter/linearRegressionTimeSeries.jl
@@ -1,1 +1,47 @@
-QQQQ - time series via linear regression.
+using CSV,DataFrames,GLM,Statistics,LinearAlgebra,Measures,Plots; pyplot()
+df = CSV.read("../data/oneOnEpsilonBlogs.csv",copycols = true)
+len = size(df)[1]
+
+dow = dayofweek.(Date.(df.Day,DateFormat("m/d/y")))
+dayGroups = [[7], [1,6], [2,3,4,5]]
+inds = [[in(d,grp) for d in dow] for grp in dayGroups]
+df2 = DataFrame(Time=1:len,Users=df.Users,
+                Group1=inds[1],Group2=inds[2])
+
+trainRange1, futureRange1 = 100:130, 130:180
+trainRange2, futureRange2 = 200:300, 300:320 
+trainRange3, futureRange3 = 400:500, 500:600 
+trainRange4, futureRange4 = 560:600, 600:630
+
+
+function forecast(trainRange,futureRange)
+    model = lm(@formula(Users ~ Time + Group1 + Group2),df2[trainRange,:])
+    pred(time) =  dot(coef(model),[1,time,inds[1][time],inds[2][time]])
+    model, pred.(trainRange), pred.(futureRange)
+end
+
+function forecastPlot(train,future)
+    p = plot(train[1], df.Users[train[1]], label = "Observed Users",
+            xlabel = "Day", ylabel = "Daily Users")
+        plot!(train[1], train[2], label = "Train")
+        plot!(future[1], future[2], label = "Forecast")
+        plot!(future[1], df.Users[future[1]], label = "Actual Users")
+    return p
+end
+
+model1, train1, fcst1 = forecast(trainRange1,futureRange1)
+println(model1)
+default(legend = :topleft)
+p1 = forecastPlot((trainRange1, train1), (futureRange1, fcst1))
+
+_, train2, fcst2 = forecast(trainRange2, futureRange2)
+default(legend = false)
+p2 = forecastPlot((trainRange2, train2), (futureRange2, fcst2))
+
+_, train3, fcst3 = forecast(trainRange3, futureRange3)
+p3 = forecastPlot((trainRange3, train3), (futureRange3, fcst3))
+
+_, train4, fcst4 = forecast(trainRange4, futureRange4)
+p4 = forecastPlot((trainRange4, train4), (futureRange4, fcst4))
+
+plot(p1, p2, p3, p4, size = (900,600), margin = 5mm)
---
+++ 8_chapter/timeSeriesEDA.jl
@@ -0,0 +1,44 @@
+using CSV, TimeSeries, Statistics, Measures, Plots, StatsPlots; pyplot()
+df = CSV.read("../data/oneOnEpsilonBlogs.csv",copycols = true)
+
+tsA = TimeArray(Date.(df.Day,DateFormat("m/d/y")),df.Users)
+tsB = moving(mean,tsA,7,padding = true)
+tsC = TimeArray(timestamp(tsA), values(tsA) - values(tsB))
+
+dow = dayofweek.(timestamp(tsA));
+dayDiv = [filter((x)->!isnan(x) && x >= -50 && x <= 50, values(tsC)[dow .== d]) 
+            for d in 1:7]
+
+start2020 = findfirst((d)->Year(d)==Year(20),timestamp(tsA))
+indexLast = length(tsA)
+indexes2020 = start2020:indexLast
+
+dayGroup1 = [7] #Sun
+dayGroup2 = [1,6] #Mon,Sat
+dayGroup3 = [2,3,4,5] #Tue, Wed, Thu, Fri
+dayGroups = [dayGroup1, dayGroup2, dayGroup3]
+groupDivs = [vcat(dayDiv[g]...) for g in dayGroups]
+
+default(legend = :topleft)
+labels = ["Daily" "7 Day Average"]
+p1 = plot(1:indexLast, [values(tsA) values(tsB)],label = labels,
+        xlabel = "Day", ylabel = "Daily Users", ylim = (0,200))
+
+p2 = plot(indexes2020, [values(tsA)[indexes2020] values(tsB)[indexes2020]],
+        label=labels,xlabel="Day in 2020",ylabel="Daily Users",ylim=(0,200))
+
+p3 = plot(1:indexLast, values(tsC),label = labels,
+        xlabel="Day",ylabel="Variation",ylim=(-50,50),legend=false)
+
+p4 = plot(indexes2020, values(tsC)[indexes2020],label = labels,
+         xlabel="Day in 2020",ylabel="Variation",ylim=(-50,50),legend=false)
+
+dayNames = dayname.(timestamp(tsA)[4:10])
+p5 = density(dayDiv, label = hcat(dayNames...),legend = :topright,
+                xlabel = "Variation", ylabel = "Frequency",)
+
+dayGroupNames = ["Sun", "Mon+Sat", "Tue+Wed+Thu+Fri"]
+p6 = density(groupDivs,label = hcat(dayGroupNames...), legend = :topright,
+                xlabel = "Variation", ylabel = "Frequency",)
+
+plot(p1, p2, p3, p4, p5, p6, layout=(3,2), size = (900,600), margin = 5mm)
