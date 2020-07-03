1b3b8af444f2cc15f76a7165dc376d0e8ba5c39c
Yoni Nazarathy <yoninazarathy@gmail.com>
2020-07-03 13:41:39 +1000

    update text

--- 9_chapter/kMeansManual.jl
+++ 9_chapter/kMeansManual.jl
@@ -1,4 +1,4 @@
-using RDatasets, Distributions, Random
+using RDatasets, Distributions, Random, LinearAlgebra
 Random.seed!(0)
 
 K = 3
@@ -10,16 +10,15 @@ shuffle!(dataPoints)
 xMin,xMax = minimum(first.(dataPoints)),maximum(first.(dataPoints))
 yMin,yMax = minimum(last.(dataPoints)),maximum(last.(dataPoints))
 
-means = [[rand(Uniform(xMin,xMax)),rand(Uniform(yMin,yMax))]  for _ in 1:k]
-labels = rand([1,k],n)
+means = [[rand(Uniform(xMin,xMax)),rand(Uniform(yMin,yMax))]  for _ in 1:K]
+labels = rand(1:K,n)
 prevMeans = -means
 
 while norm(prevMeans - means) > 0.001
     prevMeans = means
-    labels = [findmin([norm(means[i]-x) for i in 1:k])[2]  for x in dataPoints]
-    means = [sum(dataPoints[labels .== i])/sum(labels .==i) for i in 1:k]
+    labels = [argmin([norm(means[i]-x) for i in 1:K]) for x in dataPoints]
+    means = [sum(dataPoints[labels .== i])/sum(labels .==i) for i in 1:K]
 end
 
-cnts = [sum(labels .== i) for i in 1:k]
-
+cnts = [sum(labels .== i) for i in 1:K]
 println("Counts of clusters (manual implementation): ", cnts)
