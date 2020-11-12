245d9447e16183eb5f581735b3e7e8177af80957
Yoni Nazarathy <yoninazarathy@gmail.com>
2020-11-12 23:02:46 +1000

    update text

--- 1_chapter/slow.jl
+++ 1_chapter/slow.jl
@@ -1,14 +1,14 @@
 using Statistics
 
 @time begin
-        data = Float64[]
-        for _ in 1:10^6
-            group = Float64[]
-            for _ in 1:5*10^2
-                push!(group,rand())
-            end
-            push!(data,mean(group))
+    data = Float64[]
+    for _ in 1:10^6
+        group = Float64[]
+        for _ in 1:5*10^2
+            push!(group,rand())
         end
-        println("98% of the means lie in the estimated range: ",
-                        (quantile(data,0.01),quantile(data,0.99)) )
+        push!(data,mean(group))
+    end
+    println("98% of the means lie in the estimated range: ",
+                (quantile(data,0.01),quantile(data,0.99)) )
 end
